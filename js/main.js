759247bf8fb34ceadb18614f983888455b297091
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-01-05 19:51:57 +0500

    start

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const oyquyosh = document.querySelector(".oyquyosh")
+const body = document.querySelector("body")
+oyquyosh.addEventListener("click" ,()=>{
+    console.log("salom");
+    
+    body.classList.toggle("temp")
+})
