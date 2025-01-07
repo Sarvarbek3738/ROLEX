d03c879e96b80f3352e3c6118f9c9c50d27dd144
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-01-07 11:29:30 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,58 @@
-const oyquyosh = document.querySelector(".oyquyosh")
+const oyquyoshEL = document.querySelector(".oyquyosh")
 const body = document.querySelector("body")
-oyquyosh.addEventListener("click" ,()=>{
-    body.classList.toggle("temp")
-})
 
-const madaloynalar = document.querySelector(".madaloynalar")
-const chiqish = document.querySelector(".chiqish")
-chiqish.addEventListener("click",()=>{
-    madaloynalar.classList.remove("active")
-})
+var mode = localStorage.getItem("mode") ? localStorage.getItem("mode") : "quyosh";
 
 
-const madal = document.querySelector(".madal")
-madal.addEventListener("click", ()=>{
-    madaloynalar.classList.add("active")
-   
+const oyquyosh = () => {
+    if (mode == "temp") {
+        body.classList.add("temp")
+    } else {
+        body.classList.remove("temp")
+    }
+}
+oyquyosh();
+oyquyoshEL.addEventListener("click", () => {
+    if(mode == "quyosh"){
+        mode = "temp"
+    }else{
+        mode = "quyosh"
+    }
+    localStorage.setItem("mode" , mode )
+    oyquyosh()
 })
+
+
+// Elementlarni olish
+const menu = document.getElementById("menu");
+const toggleButton = document.getElementById("toggle-menu");
+const closeButton = document.getElementById("close-menu");
+
+// Lokal storage'dagi qiymatni o'qish
+const isMenuVisible = localStorage.getItem("menuVisible");
+
+// Sahifa yuklanganda menyuni holatini tekshirish
+if (isMenuVisible === "true") {
+  menu.classList.remove("hidden");
+} else {
+  menu.classList.add("hidden");
+}
+
+// Menyu tugmasini bosganda holatni o'zgartirish
+toggleButton.addEventListener("click", () => {
+  menu.classList.remove("hidden");
+  localStorage.setItem("menuVisible", "true");
+});
+
+// Menyuni yopish tugmasi
+closeButton.addEventListener("click", () => {
+  menu.classList.add("hidden");
+  localStorage.setItem("menuVisible", "false");
+});
+
+
+
+
+
+
+
