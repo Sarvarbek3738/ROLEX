6ff26f2b3c52cb388ab5a8a5da9a2ba92b6a9587
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-01-07 09:48:26 +0500

    ipdate

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ const body = document.querySelector("body")
 oyquyosh.addEventListener("click" ,()=>{
     body.classList.toggle("temp")
 })
+
+const madaloynalar = document.querySelector(".madaloynalar")
+const chiqish = document.querySelector(".chiqish")
+chiqish.addEventListener("click",()=>{
+    madaloynalar.classList.remove("active")
+})
+
+
+const madal = document.querySelector(".madal")
+madal.addEventListener("click", ()=>{
+    madaloynalar.classList.add("active")
+   
+})
